581929dcfbb8169959817c0dbaffa915f8b4fd8d
gsytec <gsytec@gmail.com>
2015-08-30 16:10:15 +0530

    first commit-1

---
+++ MavenSimpleUnitTest/src/main/java/pkg6/voda/org/MavenSimpleUnitTest/App.java
@@ -0,0 +1,13 @@
+package pkg6.voda.org.MavenSimpleUnitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
